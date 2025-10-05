6fab598f69be979f23289db9c6597419708c224e
Taka0330-jp <gtakanari0330@gmail.com>
2025-10-04 22:07:34 -0700

    change icon

--- client/src/components/BottomNav.jsx
+++ client/src/components/BottomNav.jsx
@@ -1,4 +1,10 @@
+
 import { NavLink } from "react-router-dom";
+import ChatDots from "../assets/icons/ChatDots.svg";
+import ChartBar from "../assets/icons/ChartBar.svg";
+import CalendarCheck from "../assets/icons/CalendarCheck.svg";
+import Users from "../assets/icons/Users.svg";
+import Gear from "../assets/icons/Gear.svg";
 
 export default function BottomNav() {
     const itemClass = ({ isActive }) =>
@@ -6,26 +12,26 @@ export default function BottomNav() {
         }`;
 
     return (
-        <nav className="fixed bottom-6 left-1/2 -translate-x-1/2 bg-black/40 backdrop-blur-xl rounded-full flex justify-between px-8 py-3 w-[520px] max-w-[92vw] text-sm">
+        <nav className="fixed bottom-[100px] left-1/2 -translate-x-1/2 bg-black/40 backdrop-blur-xl rounded-full flex justify-between px-8 py-3 w-[520px] max-w-[92vw] text-sm">
             <NavLink to="/dashboard" end className={itemClass}>
-                <span className="text-lg">💬</span>
-                Chat
+                <img src={ChatDots} alt="Chat" className="w-5 h-5" />
+                <span>Chat</span>
             </NavLink>
             <NavLink to="/dashboard/subscriptions" className={itemClass}>
-                <span className="text-lg">📊</span>
-                Subscriptions
+                <img src={ChartBar} alt="Subscriptions" className="w-5 h-5" />
+                <span>Subscriptions</span>
             </NavLink>
             <NavLink to="/dashboard/calendar" className={itemClass}>
-                <span className="text-lg">🗓️</span>
-                Calendar
+                <img src={CalendarCheck} alt="Calendar" className="w-5 h-5" />
+                <span>Calendar</span>
             </NavLink>
             <NavLink to="/dashboard/members" className={itemClass}>
-                <span className="text-lg">👥</span>
-                Members
+                <img src={Users} alt="Members" className="w-5 h-5" />
+                <span>Members</span>
             </NavLink>
             <NavLink to="/dashboard/setting" className={itemClass}>
-                <span className="text-lg">⚙️</span>
-                Setting
+                <img src={Gear} alt="Setting" className="w-5 h-5" />
+                <span>Setting</span>
             </NavLink>
         </nav>
     );
