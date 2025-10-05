cfea03ab42ec9ddd3aee9c64b014e79c8746e167
Taka0330-jp <gtakanari0330@gmail.com>
2025-10-04 23:52:16 -0700

    fixed chat box style

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -3,16 +3,15 @@ import ChatBox from "../components/ChatBox"; // 채팅 박스 임포트
 import BottomNav from "../components/BottomNav"; // 하단 네비게이션 임포트
 
 export default function Chat() {
-	return (
-		<div className="flex h-screen bg-white">
-			{/* 사이드바 */}
-			<SideBar />
+  return (
+    <div>
+      {/* 사이드바 */}
+      <SideBar />
 
-			{/* 메인 영역: 채팅 박스와 하단 네비 */}
-			<main className="flex-1 flex flex-col">
-				<ChatBox />
-				<BottomNav />
-			</main>
-		</div>
-	);
+      {/* 메인 영역: 채팅 박스와 하단 네비 */}
+      <ChatBox />
+      <BottomNav />
+
+    </div>
+  );
 }
